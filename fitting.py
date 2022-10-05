c701cb727b7e8a0c40a2d022ccf01fd3d5409a0a
VitalyKozinenko <vitaly.kozinenko@gmail.com>
2022-10-05 23:37:32 +0600

    Various fitting model for CH3 and CH2

--- fitting.py
+++ fitting.py
@@ -5,32 +5,45 @@ import numpy as np
 import pandas
 from models import *
 import lmfit
-from lmfit import Model, Parameters, minimize
+from lmfit import Model, Parameters, minimize, fit_report
 import corner
 from tqdm import tqdm
 
 data = pandas.read_csv("./DSS and HSA FD.csv")
 data_list = data.values.tolist()
 data_ar=np.array(data_list)
-print(data_ar[:,[0,1]])
+#print(data_ar[:,[0,1]])
 
 x=data_ar[:,0]
 #y=data_ar[:,1]
-y=1/data_ar[:,1]
-y_err=data_ar[:,2]/(data_ar[:,1]**2)
+y=1/data_ar[:,3]
+#y_err=data_ar[:,2]
+y_err=data_ar[:,4]/(data_ar[:,3]**2)
 
 gmodel = Model(R1_t_model2)
 params = Parameters()
-params.add('t_cf', value=25*1e-12, min=1*1e-12, max=100*1e-12)
-params.add('t_cb', value=40*1e-9, min=1*1e-9, max=100*1e-9)
-params.add('pbA', value=15000000, min=100, max=1e+12)
+params.add('t_cf', value=0.05*1e-9, min=1e-14, max=1e-7,vary=True)
+#params.add('t_cTMS', value=1*1e-12, min=1e-14, max=1e-7,vary=True)
+params.add('t_cb', value=40*1e-9, min=1e-14, max=1e-7,vary=True)
+params.add('pbA', value=10*1e+6, min=100, max=1e+12,vary=True)
+#params.add('pb', value=0.005, min=1e-6, max=1,vary=True)
+#params.add('A', value=4*1e+6, min=100, max=1e+12,vary=True)
 
 def residual(pars, x, data = None, eps = None):
     parvals = pars.valuesdict()
     t_cf = parvals['t_cf']
+    #t_cTMS = parvals['t_cTMS']
     t_cb = parvals['t_cb']
+    #pb = parvals['pb']
+    #A = parvals['A']
     pbA = parvals['pbA']
-    model=R1_t_model2(x,t_cf,t_cb,pbA)
+
+    #model=(R1_t_model1(x,t_cf,t_cb,pbA))
+    #model=R1_t_model2(x,t_cf,t_cTMS,t_cb,pbA)
+    #model=R1_t_model3(x,t_cf,t_cb,pb,A)
+    model=(R1_t_model1_CH2(x,t_cf,t_cb,pbA))
+    #model=(R1_t_model2_CH2(x,t_cf,t_cb,pb,A))
+    #model=(R1_t_model3_CH2(x,t_cf,t_cb,pb))
     if data is None:
         return model
     if eps is None:
@@ -38,22 +51,24 @@ def residual(pars, x, data = None, eps = None):
     return (model-data) / eps
 
 mi = minimize(residual, params, args=(x, y, y_err))
-lmfit.report_fit(mi)
+#print(mi.params.pretty_print())
+#mi1 = minimize(residual, mi.params, args=(x, y, y_err),method='leastsq')
+print(fit_report(mi))
 
-#res = minimize(residual,args=(x, y, y_err), method='emcee', burn=300, steps=2000, thin=20, params=mi.params, progress=True)
+res = minimize(residual,args=(x, y, y_err), method='emcee', burn=500, steps=2000, thin=50, params=mi.params, progress=True)
+fig1=corner.corner(res.flatchain, labels=res.var_names, truths=list(res.params.valuesdict().values()))
+fig1.set_tight_layout(True)
+fig1.savefig('Correlation.png')
 
-#result = gmodel.fit(y, params,B=x, weights=yerr,method='emcee')
-
-#corner.corner(res.flatchain, labels=res.var_names, truths=list(res.params.valuesdict().values()),title_kwargs={"fontsize": 18}, smooth=True)
-
-fig, ax = plt.subplots()
+fig2, ax = plt.subplots()
 #ax.semilogx(x, y, 'o')
 ax.errorbar(x, y, yerr = y_err,marker='o',capsize=5)
 x1 = np.logspace(-3, 1.5, 100)
 ax.semilogx(x1, residual(mi.params,x1), '-', label='best fit')
-#ax.semilogx(x, result.init_fit, '--', label='initial fit')
+#ax.semilogx(x1, residual(params,x1), '--', label='initial fit')
 ax.set_title('T1 relaxation FD')
 ax.set_xlabel('B, T')
-ax.set_ylabel('Total T_1, s')
+ax.set_ylabel('R1 relaxation rate, 1/s')
 ax.legend()
+fig2.savefig('Fitting.png')
 plt.show()
--- models.py
+++ models.py
@@ -14,7 +14,7 @@ J_HH = 17.2;
 delta_CS = 0.11*1e-6;
 
 #Dipole-dipole parameters
-r_hh=1.8e-10;
+r_hh=179*1e-12;
 b_hh1=-mu0/(4*pi)*h_bar*gamma**2/r_hh**3;
 b_hh2=-2*pi*10000;
 
@@ -47,12 +47,68 @@ def T1_t_model1(B,t_cf,t_cb,pbA):
 
     return 1/(3/10*b_hh1**2*(J(w,t_cf)+4*J(2*w,t_cf))+1/10*pbA*(J(0,t_cb)+3*J(w,t_cb)+6*J(2*w,t_cb)))
 
-     #Total T1 relaxation model from Ziqing Wang 2021
-def R1_t_model2(B,t_cf,t_cb,pbA):
+     
+def R1_t_model1(B,t_cf,t_cb,pbA): #Total T1 relaxation Model 1 from Ziqing Wang 2021 - intra dipole in free and inter dipole in bound
     w=B*gamma
-    b_hh=-mu0/pi*h_bar*gamma**2/r_hh**3;
+    b_hh=-mu0/pi*h_bar*gamma**2/r_hh**3
+    S2tms=1/4
+    Amethyl=1/4*3/32*b_hh**2
 
     def J(w,t_c): 
         return 2/5*t_c/(1+w**2*t_c**2)
 
-    return 1/4*1/4*3/32*b_hh**2*(J(w,t_cf)+4*J(2*w,t_cf))+pbA*(J(0,t_cb)+3*J(w,t_cb)+6*J(2*w,t_cb))
+    return S2tms*Amethyl*(J(w,t_cf)+4*J(2*w,t_cf))+pbA*(J(0,t_cb)+3*J(w,t_cb)+6*J(2*w,t_cb))
+
+def R1_t_model2(B,t_cf,t_cTMS,t_cb,pbA): #Total T1 relaxation Model 2 from Ziqing Wang 2021 - TMS rotation in free form
+    w=B*gamma
+    b_hh=-mu0/pi*h_bar*gamma**2/r_hh**3
+    S2tms=1/4
+    Amethyl=1/4*3/32*b_hh**2
+
+    t_c1=(t_cf*t_cTMS)/(t_cf+t_cTMS)
+
+    def J(w,t_c): 
+        return 2/5*t_c/(1+w**2*t_c**2)
+
+    return Amethyl*(S2tms*(J(w,t_cf)+4*J(2*w,t_cf))+(1-S2tms)*(J(w,t_c1)+4*J(2*w,t_c1)))+pbA*(J(0,t_cb)+3*J(w,t_cb)+6*J(2*w,t_cb))
+
+def R1_t_model3(B,t_cf,t_cb,pb,A): #Total T1 relaxation Model 3 from Ziqing Wang 2021 - finite concentration of bound
+    w=B*gamma
+    b_hh=-mu0/pi*h_bar*gamma**2/r_hh**3
+    S2tms=1/4
+    Amethyl=1/4*3/32*b_hh**2
+
+    def J(w,t_c): 
+        return 2/5*t_c/(1+w**2*t_c**2)
+
+    return (1-pb)*S2tms*Amethyl*(J(w,t_cf)+4*J(2*w,t_cf))+pb*(A*(J(0,t_cb)+3*J(w,t_cb)+6*J(2*w,t_cb))+S2tms*Amethyl*(J(w,t_cb)+4*J(2*w,t_cb)))
+
+def R1_t_model1_CH2(B,t_cf,t_cb,pbA): #Total T1 relaxation Model 1 for CH2 group
+    w=B*gamma
+    b_hh=-mu0/pi*h_bar*gamma**2/r_hh**3
+    A_CH2=3/32*b_hh**2
+
+    def J(w,t_c): 
+        return 2/5*t_c/(1+w**2*t_c**2)
+
+    return A_CH2*(J(w,t_cf)+4*J(2*w,t_cf))+pbA*(J(0,t_cb)+3*J(w,t_cb)+6*J(2*w,t_cb))
+
+def R1_t_model2_CH2(B,t_cf,t_cb,pb,A): #Total T1 relaxation Model 2 for CH2 group
+    w=B*gamma
+    b_hh=-mu0/pi*h_bar*gamma**2/r_hh**3
+    A_CH2=3/32*b_hh**2
+
+    def J(w,t_c): 
+        return 2/5*t_c/(1+w**2*t_c**2)
+
+    return (1-pb)*A_CH2*(J(w,t_cf)+4*J(2*w,t_cf))+pb*(A*(J(0,t_cb)+3*J(w,t_cb)+6*J(2*w,t_cb))+A_CH2*(J(w,t_cb)+4*J(2*w,t_cb)))
+
+def R1_t_model3_CH2(B,t_cf,t_cb,pb): #Total T1 relaxation Model 3 for CH2 group
+    w=B*gamma
+    b_hh=-mu0/pi*h_bar*gamma**2/r_hh**3
+    A_CH2=3/32*b_hh**2
+
+    def J(w,t_c): 
+        return 2/5*t_c/(1+w**2*t_c**2)
+
+    return (1-pb)*A_CH2*(J(w,t_cf)+4*J(2*w,t_cf))+pb*A_CH2*(J(w,t_cb)+4*J(2*w,t_cb))
