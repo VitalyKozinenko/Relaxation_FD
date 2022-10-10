f3887f1be489e319c586ef4a5c0e6de98bd4d0d2
VitalyKozinenko <vitaly.kozinenko@gmail.com>
2022-10-10 10:25:36 +0600

    All the actual models

--- fitting_multi.py
+++ fitting_multi.py
@@ -16,9 +16,11 @@ data_ar=np.array(data_list)
 
 x=data_ar[:,0]
 #y=1/data_ar[:,[5,9,13,17]]
-y=data_ar[:,[5,9,13,17]]
+#y=data_ar[:,[5,9,13,17]] #T1 relaxation data
+y=data_ar[:,[7,11,15,19]] #T1 relaxation data
 #y_err=data_ar[:,[6,10,14,18]]/(data_ar[:,[5,9,13,17]]**2)
-y_err=data_ar[:,[6,10,14,18]]
+#y_err=data_ar[:,[6,10,14,18]] #T1 relaxation data error
+y_err=data_ar[:,[8,12,16,20]] #T1 relaxation data error
 #print(y_err)
 
 print('data\n')
@@ -36,7 +38,10 @@ def model_dataset(params, i, x):
     s2 = params[f's2_{i+1}']
     pb = params[f'pb_{i+1}']
     #A = params[f'A_{i+1}']
-    return 1/(R1_t_model4_CH2(x,t_cf,t_cb,t_cm,s2,pb))
+    #pbA = params[f'pbA_{i+1}']
+    Rslow = params[f'Rslow_{i+1}']
+    #Rslow2 = params[f'Rslow2_{i+1}']
+    return 1/(Rs_t_model5_CH2(x,t_cf,t_cb,t_cm,pb,Rslow,s2))
 
 def residual(params, x, data, eps = None):
     #Calculate total residual for fits of R1 to several data sets.
@@ -60,9 +65,12 @@ for iy, y in enumerate(data_y):
     params.add(f't_cf_{iy+1}', value=0.05*1e-9, min=1e-14, max=1e-7)
     params.add(f't_cb_{iy+1}', value=40*1e-9, min=1e-14, max=1e-7)
     params.add(f't_cm_{iy+1}', value=1*1e-9, min=1e-14, max=1e-7)
-    params.add(f's2_{iy+1}', value=0.5, min=1e-16, max=1)
-    params.add(f'pb_{iy+1}', value=1e-1, min=1e-9, max=1)    
+    params.add(f's2_{iy+1}', value=0.1, min=1e-16, max=1)
+    params.add(f'pb_{iy+1}', value=1e-4, min=1e-9, max=1)    
     #params.add(f'A_{iy+1}', value=1e+6, min=1e+3, max=1e+16)
+    #params.add(f'pbA_{iy+1}', value=1e+6, min=1e+3, max=1e+16)
+    params.add(f'Rslow_{iy+1}', value=1e-1, min=1e-4, max=1e+3)
+    #params.add(f'Rslow2_{iy+1}', value=1e+2, min=1e-4, max=1e+3)
 
 for iy in (2, 3, 4):
     params[f't_cf_{iy}'].expr = 't_cf_1'
@@ -70,77 +78,37 @@ for iy in (2, 3, 4):
     params[f't_cm_{iy}'].expr = 't_cm_1'
     params[f's2_{iy}'].expr = 's2_1'
     #params[f'A_{iy}'].expr = 'A_1'
+    params[f'Rslow_{iy}'].expr = 'Rslow_1'
+    #params[f'Rslow2_{iy}'].expr = 'Rslow2_1'
 
+#out = minimize(residual, params, args=(x, data_y),method='differential_evolution')
 out = minimize(residual, params, args=(x, data_y),method='leastsq')
 report_fit(out.params)
 
+print('-------------------------------')
+print('Parameter    Value       Stderr')
+for name, param in out.params.items():
+    print(f'{name:7s} {param.value:.2E} {param.stderr:.2E}')
+
 ###############################################################################
 # Plot the data sets and fits
-fig1, ax = plt.subplots()
+fig1 = plt.figure(figsize=(10, 6),dpi=100)
+gs = fig1.add_gridspec(1,4, wspace=0)
+axs = gs.subplots(sharex=True, sharey=True)
 x1 = np.logspace(-3, 1.5, 100)
 for i in range(4):
     y_fit = model_dataset(out.params, i, x1)
-    ax.semilogx(x1, y_fit, '-')
-    ax.errorbar(x, data_y[i, :], yerr = data_err[i, :],marker='o',capsize=5)
+    axs[i].semilogx(x1, y_fit, '-')
+    axs[i].errorbar(x, data_y[i, :], yerr = data_err[i, :],marker='o',capsize=5)
 
-ax.set_title('T1 relaxation FD')
-ax.set_xlabel('B, T')
-ax.set_ylabel('T1 relaxation time, s')
+axs[0].set_ylabel('Tlls relaxation time, s')
+
+for ax in axs.flat:
+    ax.set(xlabel='B, T')
+
+fig1.suptitle('Tlls relaxation FD')
+#ax.set_xlabel('B, T')
+#ax.set_ylabel('T1 relaxation time, s')
 fig1.savefig('Fitting.png')
 plt.show()
 
-
-"""
-#params.add('t_cf', value=0.05*1e-9, min=1e-14, max=1e-7,vary=True)
-#params.add('t_cTMS', value=1*1e-12, min=1e-14, max=1e-7,vary=True)
-#params.add('t_cb', value=40*1e-9, min=1e-14, max=1e-7,vary=True)
-#params.add('pbA', value=10*1e+6, min=100, max=1e+12,vary=True)
-#params.add('pb', value=0.005, min=1e-6, max=1,vary=True)
-#params.add('A', value=4*1e+6, min=100, max=1e+12,vary=True)
-
-def residual(pars, x, data = None, eps = None):
-    parvals = pars.valuesdict()
-    t_cf = parvals['t_cf']
-    #t_cTMS = parvals['t_cTMS']
-    #t_cb = parvals['t_cb']
-    #pb = parvals['pb']
-    #A = parvals['A']
-    #pbA = parvals['pbA']
-
-    #model=(R1_t_model1(x,t_cf,t_cb,pbA))
-    #model=R1_t_model2(x,t_cf,t_cTMS,t_cb,pbA)
-    #model=R1_t_model3(x,t_cf,t_cb,pb,A)
-    #model=(R1_t_model1_CH2(x,t_cf,t_cb,pbA))
-    #model=(R1_t_model2_CH2(x,t_cf,t_cb,pb,A))
-    #model=(R1_t_model3_CH2(x,t_cf,t_cb,pb))
-    #model=(R1_model_freeCH2(x,t_cf))
-    if data is None:
-        return model
-    if eps is None:
-        return model - data
-    return (model-data) / eps
-
-mi = minimize(residual, params, args=(x, y, y_err))
-#print(mi.params.pretty_print())
-#mi1 = minimize(residual, mi.params, args=(x, y, y_err),method='leastsq')
-print(fit_report(mi))
-
-#res = minimize(residual,args=(x, y, y_err), method='emcee', burn=500, steps=2000, thin=50, params=mi.params, progress=True)
-#fig1=corner.corner(res.flatchain, labels=res.var_names, truths=list(res.params.valuesdict().values()))
-#fig1.set_tight_layout(True)
-#fig1.savefig('Correlation.png')
-
-fig2, ax = plt.subplots()
-#ax.semilogx(x, y, 'o')
-ax.errorbar(x, y, yerr = y_err,marker='o',capsize=5)
-x1 = np.logspace(-3, 1.5, 100)
-ax.semilogx(x1, residual(mi.params,x1), '-', label='best fit')
-#ax.semilogx(x1, residual(params,x1), '--', label='initial fit')
-ax.set_title('T1 relaxation FD')
-ax.set_xlabel('B, T')
-ax.set_ylabel('R1 relaxation rate, 1/s')
-ax.legend()
-ax.set(ylim=(0, 1))
-fig2.savefig('Fitting.png')
-plt.show()
-"""
--- models.py
+++ models.py
@@ -28,6 +28,10 @@ b_hh2=-2*pi*10000;
 def J1(w,t_c): 
     return t_c/(1+w**2*t_c**2)
 
+####################################################
+# T1 relaxation models 
+####################################################
+
 #R1 relaxation in free
 def R1_f(B,t_c,J):
     w=B*gamma
@@ -38,15 +42,9 @@ def R1_b(B,t_c,J):
     w=B*gamma
     return 1/10*b_hh1**2*(J(0,t_c)+3*J(w,t_c)+6*J(2*w,t_c))
 
-#Total T1 relaxation model 1
-def T1_t_model1(B,t_cf,t_cb,pbA):
-    w=B*gamma
-
-    def J(w,t_c): 
-        return t_c/(1+w**2*t_c**2)
-
-    return 1/(3/10*b_hh1**2*(J(w,t_cf)+4*J(2*w,t_cf))+1/10*pbA*(J(0,t_cb)+3*J(w,t_cb)+6*J(2*w,t_cb)))
-
+#########################
+# Models for CH3
+#########################
      
 def R1_t_model1(B,t_cf,t_cb,pbA): #Total T1 relaxation Model 1 from Ziqing Wang 2021 - intra dipole in free and inter dipole in bound
     w=B*gamma
@@ -83,7 +81,11 @@ def R1_t_model3(B,t_cf,t_cb,pb,A): #Total T1 relaxation Model 3 from Ziqing Wang
 
     return (1-pb)*S2tms*Amethyl*(J(w,t_cf)+4*J(2*w,t_cf))+pb*(A*(J(0,t_cb)+3*J(w,t_cb)+6*J(2*w,t_cb))+S2tms*Amethyl*(J(w,t_cb)+4*J(2*w,t_cb)))
 
-def R1_t_model1_CH2(B,t_cf,t_cb,pbA): #Total T1 relaxation Model 1 for CH2 group
+#########################
+# Models for CH2
+#########################
+
+def R1_t_model1_CH2(B,t_cf,t_cb,pbA): #Total T1 relaxation Model 1 for CH2 group - intra in free and inter in bound
     w=B*gamma
     b_hh=-mu0/(4*pi)*h_bar*gamma**2/r_hh**3
 
@@ -93,7 +95,7 @@ def R1_t_model1_CH2(B,t_cf,t_cb,pbA): #Total T1 relaxation Model 1 for CH2 group
     return 3/10*b_hh**2*(J(w,t_cf)+4*J(2*w,t_cf))+pbA*(J(0,t_cb)+3*J(w,t_cb)+6*J(2*w,t_cb))
 
 
-def R1_t_model2_CH2(B,t_cf,t_cb,pb,A): #Total T1 relaxation Model 2 for CH2 group
+def R1_t_model2_CH2(B,t_cf,t_cb,pb,A): #Total T1 relaxation Model 2 for CH2 group - intra in free and inter and intra in bound
     w=B*gamma
     b_hh=-mu0/(4*pi)*h_bar*gamma**2/r_hh**3
 
@@ -102,7 +104,7 @@ def R1_t_model2_CH2(B,t_cf,t_cb,pb,A): #Total T1 relaxation Model 2 for CH2 grou
 
     return 3/10*b_hh**2*(J(w,t_cf)+4*J(2*w,t_cf))+pb*(A*(J(0,t_cb)+3*J(w,t_cb)+6*J(2*w,t_cb))+3/10*b_hh**2*(J(w,t_cb)+4*J(2*w,t_cb)))
 
-def R1_t_model3_CH2(B,t_cf,t_cb,pb): #Total T1 relaxation Model 3 for CH2 group
+def R1_t_model3_CH2(B,t_cf,t_cb,pb): #Total T1 relaxation Model 3 for CH2 group - intra in free and bound
     w=B*gamma
     b_hh=-mu0/(4*pi)*h_bar*gamma**2/r_hh**3
 
@@ -111,7 +113,7 @@ def R1_t_model3_CH2(B,t_cf,t_cb,pb): #Total T1 relaxation Model 3 for CH2 group
 
     return 3/10*b_hh**2*(J(w,t_cf)+4*J(2*w,t_cf))+pb*3/10*b_hh**2*(J(w,t_cb)+4*J(2*w,t_cb))
 
-def R1_t_model4_CH2(B,t_cf,t_cb,t_cm,s2,pb): #Total T1 relaxation Model 3 for CH2 group
+def R1_t_model4_CH2(B,t_cf,t_cb,t_cm,s2,pb): #Total T1 relaxation Model 4 for CH2 group - intra in free and intra in bound with order parameter S^2 and correlation time t_cm
     w=B*gamma
     b_hh=-mu0/(4*pi)*h_bar*gamma**2/r_hh**3
 
@@ -122,6 +124,19 @@ def R1_t_model4_CH2(B,t_cf,t_cb,t_cm,s2,pb): #Total T1 relaxation Model 3 for CH
 
     return 3/10*b_hh**2*(J(w,t_cf)+4*J(2*w,t_cf))+pb*3/10*b_hh**2*(s2*(J(w,t_cb)+4*J(2*w,t_cb))+(1-s2)*(J(w,t_cb1)+4*J(2*w,t_cb1)))
 
+def R1_t_model5_CH2(B,t_cf,t_cb,t_cm,s2,pb,A): #Total T1 relaxation Model 5 for CH2 group - intra in free and intra and inter in bound with order parameter S^2 and correlation time t_cm
+    w=B*gamma
+    b_hh=-mu0/(4*pi)*h_bar*gamma**2/r_hh**3
+
+    t_cb1=(t_cb*t_cm)/(t_cb+t_cm)
+
+    def J(w,t_c): 
+        return t_c/(1+w**2*t_c**2)
+
+    return 3/10*b_hh**2*(J(w,t_cf)+4*J(2*w,t_cf))+pb*(3/10*b_hh**2*(s2*(J(w,t_cb)+4*J(2*w,t_cb))+(1-s2)*(J(w,t_cb1)+4*J(2*w,t_cb1)))
+            +A*(s2*(J(0,t_cb)+3*J(w,t_cb)+6*J(2*w,t_cb))+(1-s2)*(J(0,t_cb1)+3*J(w,t_cb1)+6*J(2*w,t_cb1))))
+
+
 def R1_model_freeCH2(B,t_cf): #Total T1 relaxation Model 3 for CH2 group
     w=B*gamma
     b_hh=-mu0/(4*pi)*h_bar*gamma**2/r_hh**3
@@ -130,3 +145,89 @@ def R1_model_freeCH2(B,t_cf): #Total T1 relaxation Model 3 for CH2 group
         return t_c/(1+w**2*t_c**2)
 
     return 3/10*b_hh**2*(J(w,t_cf)+4*J(2*w,t_cf))
+
+####################################################
+# Tlls relaxation models 
+####################################################
+
+def Rs_t_model1_CH2(B,t_cf,t_cb,pbA,Rslow): 
+    w=B*gamma
+    b_hh=-mu0/(4*pi)*h_bar*gamma**2/r_hh**3
+    theta=np.arctan((2*pi*J_HH)/(w*delta_CS))/2
+
+    def J(w,t_c): 
+        return t_c/(1+w**2*t_c**2)
+
+    def R1(t_c):
+        return 3/2*t_c*b_hh**2
+
+    def Rs(Rslow,t_c):
+        return (20+2*np.cos(4*theta)- np.sqrt(2*(81-80*np.cos(4*theta)+np.cos(8*theta))))/60*R1(t_c)+Rslow
+
+    return Rs(Rslow,t_cf)+pbA*(J(0,t_cb)+3*J(w,t_cb)+6*J(2*w,t_cb))
+
+def Rs_t_model2_CH2(B,t_cf,t_cb,pb,Rslow):
+    w=B*gamma
+    b_hh=-mu0/(4*pi)*h_bar*gamma**2/r_hh**3
+    theta=np.arctan((2*pi*J_HH)/(w*delta_CS))/2
+
+    def J(w,t_c): 
+        return t_c/(1+w**2*t_c**2)
+
+    def R1(t_c):
+        return 3/2*t_c*b_hh**2
+
+    def Rs(Rslow,t_c):
+        return (20+2*np.cos(4*theta)- np.sqrt(2*(81-80*np.cos(4*theta)+np.cos(8*theta))))/60*R1(t_c)+Rslow
+
+    return Rs(Rslow,t_cf)+pb*3/10*b_hh**2*(J(w,t_cb)+4*J(2*w,t_cb))
+
+def Rs_t_model3_CH2(B,t_cf,t_cb,A,pb,Rslow1): 
+    w=B*gamma
+    b_hh=-mu0/(4*pi)*h_bar*gamma**2/r_hh**3
+    theta=np.arctan((2*pi*J_HH)/(w*delta_CS))/2
+
+    def J(w,t_c): 
+        return t_c/(1+w**2*t_c**2)
+
+    def R1(t_c):
+        return 3/10*b_hh**2*(J(w,t_c)+4*J(2*w,t_c))
+
+    def Rs(Rslow,t_c):
+        return (20+2*np.cos(4*theta)- np.sqrt(2*(81-80*np.cos(4*theta)+np.cos(8*theta))))/60*R1(t_c)+Rslow
+
+    return Rs(Rslow1,t_cf)+pb*(A*(J(0,t_cb)+3*J(w,t_cb)+6*J(2*w,t_cb))+ Rs(Rslow1,t_cb))
+
+def Rs_t_model4_CH2(B,t_cf,t_cb,t_cm,pb,Rslow,s2): 
+    w=B*gamma
+    b_hh=-mu0/(4*pi)*h_bar*gamma**2/r_hh**3
+    theta=np.arctan((2*pi*J_HH)/(w*delta_CS))/2
+    t_cb1=(t_cb*t_cm)/(t_cb+t_cm)
+
+    def J(w,t_c): 
+        return t_c/(1+w**2*t_c**2)
+
+    def R1(t_c):
+        return 3/2*t_c*b_hh**2
+
+    def Rs(Rslow,t_c):
+        return (20+2*np.cos(4*theta)- np.sqrt(2*(81-80*np.cos(4*theta)+np.cos(8*theta))))/60*R1(t_c)+Rslow
+
+    return Rs(Rslow,t_cf)+pb*3/10*b_hh**2*(s2*(J(w,t_cb)+4*J(2*w,t_cb))+(1-s2)*(J(w,t_cb1)+4*J(2*w,t_cb1)))
+
+def Rs_t_model5_CH2(B,t_cf,t_cb,t_cm,pb,Rslow,s2): 
+    w=B*gamma
+    b_hh=-mu0/(4*pi)*h_bar*gamma**2/r_hh**3
+    theta=np.arctan((2*pi*J_HH)/(w*delta_CS))/2
+    t_cb1=(t_cb*t_cm)/(t_cb+t_cm)
+
+    def J(w,t_c): 
+        return t_c/(1+w**2*t_c**2)
+
+    def R1(t_c):
+        return 3/10*b_hh**2*(J(w,t_c)+4*J(2*w,t_c))
+
+    def Rs(Rslow,t_c):
+        return (20+2*np.cos(4*theta)- np.sqrt(2*(81-80*np.cos(4*theta)+np.cos(8*theta))))/60*R1(t_c)+Rslow
+
+    return Rs(Rslow,t_cf)+pb*(s2*(Rs(Rslow,t_cb)+3/10*b_hh**2*(J(w,t_cb)+4*J(2*w,t_cb)))+(1-s2)*(Rs(Rslow,t_cb1)+3/10*b_hh**2*(J(w,t_cb1)+4*J(2*w,t_cb1))))
